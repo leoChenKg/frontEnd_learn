408d7f9cf31f53ee2a3d00b2c5b5f067700cc248
leoChenKg <okcchampion1@gmail.com>
2022-08-07 16:44:02 +0800

    完善页面断点监听hhook

--- notes/src/layout/home/Home.jsx
+++ notes/src/layout/home/Home.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import  './home.module.css'
+import './home.module.css'
+import { useSeparator } from '@utils/separator'
 
 export default function Home() {
-  const [count, setCount] = useState(1)
+  const styles = useSeparator({
+    color: {
+      xs: 'red',
+      sm: 'blue',
+      lg: 'green'
+    },
+    border: {
+      lg: '1px solid green',
+      xl: '1px solid blue'
+    }
+  })
+
+  console.log(styles)
 
   return (
     <div>
+      <div style={styles}>test</div>
       <h2>FrontEnd Learn...</h2>
-      <div>{count}</div>
-      <button onClick={() => setCount(x => x + 1)}>click</button>
     </div>
   )
 }
