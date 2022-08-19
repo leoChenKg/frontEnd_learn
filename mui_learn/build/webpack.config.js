e3d7b5fe8375783514c10961d8ec1cb3a148e3da
leoChenKg <okcchampion1@gmail.com>
2022-08-19 22:10:07 +0800

    mui learn

---
+++ mui_learn/build/webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+
+const BaseConfig = () => {
+  return {
+    devtool: 'source-map',
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /(node_modules)/,
+          use: ['babel-loader', 'ts-loader']
+        },
+        {
+          test: /\.css$/i,
+          exclude: /(node_modules)/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [['postcss-preset-env']]
+                }
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(scss|sass)$/i,
+          exclude: /(node_modules)/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [['postcss-preset-env']]
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        },
+
+        {
+          test: /\.(woff|woff2|eot|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[name].[contenthash:8].[ext]'
+          }
+        },
+        {
+          test: /\.(png|jpg|jpng|gif|svg)$/i,
+          type: 'asset',
+          parser: {
+            dataUrlCondition: {
+              maxSize: 4 * 1024
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlPlugin({
+        template: path.resolve(__dirname, '../index.html')
+      })
+    ],
+    devServer: {
+      port: 3000,
+      historyApiFallback: true
+    }
+  }
+}
+
+module.exports = BaseConfig
---
+++ mui_learn/src/index.ts
@@ -0,0 +1,8 @@
+interface A {
+  name: string
+}
+const a: A = {
+  name: 'leo'
+}
+
+console.log(a)
